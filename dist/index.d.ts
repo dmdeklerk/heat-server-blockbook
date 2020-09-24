edc7d32eb33cbabe4c56f5167ba35816cc9d914d
dmdeklerk <dennis@heatledger.com>
2020-09-25 00:49:57 +0200

    Export middleware

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,1 +1,8 @@
 export * from './explorer';
+export declare const BLOCKBOOK_ETH_NETWORK_FEE: (input: any) => {
+    gasPriceWei: string;
+};
+export declare const BLOCKBOOK_UTXO_NETWORK_FEE: (input: any) => {
+    satByte: string;
+};
+export declare const BLOCKBOOK_LTC_GETADDRESS: (address: any) => any;
--- dist/index.js
+++ dist/index.js
@@ -10,5 +10,39 @@ var __exportStar = (this && this.__exportStar) || function(m, exports) {
     for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.BLOCKBOOK_LTC_GETADDRESS = exports.BLOCKBOOK_UTXO_NETWORK_FEE = exports.BLOCKBOOK_ETH_NETWORK_FEE = void 0;
+const big_js_1 = require("big.js");
+const bitcoinjs_lib_1 = require("bitcoinjs-lib");
 __exportStar(require("./explorer"), exports);
+const WEI = new big_js_1.default(10).pow(18);
+exports.BLOCKBOOK_ETH_NETWORK_FEE = function (input) {
+    console.log(`WEI = ${WEI}`);
+    return {
+        gasPriceWei: new big_js_1.default(input).times(WEI).toString(),
+    };
+};
+const SATOSHI = new big_js_1.default(10).pow(8);
+exports.BLOCKBOOK_UTXO_NETWORK_FEE = function (input) {
+    console.log(`SATOSHI = ${SATOSHI}, input = ${input}`);
+    return {
+        satByte: new big_js_1.default(input)
+            .times(SATOSHI)
+            .div(1000)
+            .round()
+            .toString(),
+    };
+};
+exports.BLOCKBOOK_LTC_GETADDRESS = function (address) {
+    try {
+        if ((address || '').startsWith('M'))
+            return address;
+        let decoded = bitcoinjs_lib_1.address.fromBase58Check(address);
+        let version = decoded['version'];
+        return bitcoinjs_lib_1.address.toBase58Check(decoded['hash'], version);
+    }
+    catch (err) {
+        console.error(err);
+    }
+    return address;
+};
 //# sourceMappingURL=index.js.map
